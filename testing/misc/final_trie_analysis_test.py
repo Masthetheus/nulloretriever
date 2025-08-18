f93f73e0d7506b9699c9b2918cd2e53966dc802c
matheus <matheuspedroncassol@gmail.com>
2025-08-18 17:54:42 -0300

    scripts: initial work on post generation analysis

---
+++ testing/misc/final_trie_analysis_test.py
@@ -0,0 +1,11 @@
+from funcoes_mestrado import processar_null_batch
+import os
+import csv
+import pandas as pd
+
+csv_total_null = '/home/leveduras/integranteslab/matheus/resultados_novos/total_nulomeros.csv'
+csv_db_orgs = "/home/leveduras/integranteslab/matheus/db/teste.tsv"
+base_path = '/home/leveduras/integranteslab/matheus/resultados_novos/'
+config_file = '/home/leveduras/integranteslab/matheus/Mestradoteste/Scripts/snakemakefluxo_c_trie_bit/config/config.yaml'
+
+processar_null_batch(csv_total_null, base_path, config_file)
