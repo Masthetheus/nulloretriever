48160c593e1c5bf1b8a394d3a1a62376385d8604
Matheus <matheuspedroncassol@gmail.com>
2025-11-03 17:24:01 -0300

    c: modularization

--- workflow/scripts/c/kmer_deduplication.c
+++ workflow/scripts/c/kmer_deduplication.c
@@ -1,14 +1,21 @@
 #include "../h/kmer_deduplication.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <inttypes.h>
+
 char* bitarray_deduplication(int k){
   uint64_t total = 1ULL << (2*k);
   char* seen = calloc((total+7) / 8, 1);
   return seen;
 }
 
-void deduplication_method(int k){
-  if(k>=8 && k<=16)
-    bitarray_deduplication(k);
+char* deduplication_method(int k){
+  if(k>=8 && k<=16){
+    return bitarray_deduplication(k);
+  }
   else if(k <= 20)
     chainhash_deduplication()
   else
