2d9a6e4f8b0cf5705a28571013a6caa99c37fcbc
Matheus <matheuspedroncassol@gmail.com>
2026-04-28 17:50:53 -0300

    scripts: comparison test mixed results.

--- nulloretriever/analysis/parsings.py
+++ nulloretriever/analysis/parsings.py
@@ -1,6 +1,7 @@
 """Defines function that parses files aiming downstream analysis."""
 
 import struct
+from collections import defaultdict
 
 
 def obtain_trie_infos(filename):
@@ -77,7 +78,7 @@ def sequence_length_conversion(half_k, v1=False, v2=False):
 def obtain_v1_positions(file):
     """Retrieves the location of the v1 indexes inside a bit trie file."""
     byte_to_format = {1: 'B', 2: 'H', 4: 'I', 8: 'Q'}
-    v1_locations = {}
+    v1_locations = defaultdict(set)
     with open(file, 'rb') as f:
         # Skip header
         f.seek(8)
@@ -124,3 +125,30 @@ def gather_v1_related_data(v1_bit_index, file):
         except (struct.error, OSError):
             pass
     return v2s
+
+
+def obtain_nullomer_set(file, v1_positions):
+    """Retrieves the collection of v1 and v2 from a nullomer bit file."""
+    byte_size, byte_format = obtain_trie_infos(file)
+    v1_count = 0
+    nullomer_set = defaultdict(set)
+    with open(file, 'rb') as f:
+        for v1, position in v1_positions.items():
+            f.seek(position, 0)
+            counter = 0
+            try:
+                nullomer_count_bytes = f.read(byte_size)
+                if len(nullomer_count_bytes) < byte_size:
+                    break
+                nullomer_count = struct.unpack(
+                    f'<{byte_format}', nullomer_count_bytes)[0]
+                while counter < nullomer_count:
+                    v2_bytes = f.read(byte_size)
+                    nullomer_set[v1].add(struct.unpack(
+                        f'<{byte_format}', v2_bytes)[0])
+                    counter += 1
+            except (struct.error, OSError) as e:
+                print(e)
+                pass
+            v1_count += 1
+    return nullomer_set
