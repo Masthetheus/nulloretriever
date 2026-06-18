22024bb7238b7345f1cbd428963dd96aa35697b5
Matheus <matheuspedroncassol@gmail.com>
2026-06-18 18:45:23 -0300

    scripts:tests

--- workflow/scripts/c/kmer_encoding.c
+++ workflow/scripts/c/kmer_encoding.c
@@ -7,11 +7,15 @@
 #include <string.h>
 
 uint64_t encode_kmer(const char *seq, int k) {
-        uint64_t val = 0;
+        uint64_t val = 0, revval = 0;
         for (int i = 0; i < k; i++) {
                 val <<= 2;
                 val |= (seq[i]>>1) & 3; 
+                revval |= ((seq[i]>>1) & 3) ^ 2; 
+                printf("revval: %ld.", revval);
+                revval >>= 2;
                 }
+        printf("FINAL revval: %ld.", revval);
         return val;
 }
 
@@ -21,8 +25,6 @@ uint64_t encode_rev(const char *seq, int k, int seqlen, int cont) {
         for (int i = 0; i < k; i++) {
                 val <<= 2;
                 uint64_t base = (seq[seqlen - i - cont] >> 1) & 3;
-                seqlen -= k;
-                printf("Seq %d, Orig %d, base %ld, val %ld\n", seq[seqlen-i-cont],seq[i], base,base^2);
                 val |= base ^ 2; 
                 }
         return val;
