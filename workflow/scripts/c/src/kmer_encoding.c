0bfccbd1fe52896e8e833d113a9278c86ff684e3
Matheus <matheuspedroncassol@gmail.com>
2026-07-08 12:55:06 -0300

    added skipping for N bases

--- workflow/scripts/c/src/kmer_encoding.c
+++ workflow/scripts/c/src/kmer_encoding.c
@@ -6,13 +6,28 @@
 #include <stdlib.h>
 #include <string.h>
 
+static int8_t base_to_bits[256];
+
+void init_base_table(void) {
+    // Set all entries to -1 (invalid)
+    memset(base_to_bits, -1, sizeof(base_to_bits));
+
+    base_to_bits['A'] = base_to_bits['a'] = 0;
+    base_to_bits['C'] = base_to_bits['c'] = 1;
+    base_to_bits['T'] = base_to_bits['t'] = 2;
+    base_to_bits['G'] = base_to_bits['g'] = 3;
+}
 uint64_t encode_kmer(const char *seq, int k) {
-        uint64_t val = 0;
-        for (int i = 0; i < k; i++) {
-                val <<= 2;
-                val |= (seq[i]>>1) & 3; 
-                }
-        return val;
+    init_base_table();
+    uint64_t val = 0;
+    for (int i = 0; i < k; i++) {
+        int bits = base_to_bits[(unsigned char)seq[i]];
+        if (bits < 0) {   // invalid character
+            return UINT64_MAX;
+        }
+        val = (val << 2) | bits;
+    }
+    return val;
 }
 
 uint64_t encode_rev(uint64_t val, int k) {
